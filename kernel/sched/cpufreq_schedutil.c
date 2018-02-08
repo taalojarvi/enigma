57f992661780023bb30de4335c9e80504ba1c4c3
Leo Yan <leo.yan@linaro.org>
2018-02-08 21:48:22 +0800

    sched/cpufreq: Remove unused SUGOV_KTHREAD_PRIORITY macro
    Since schedutil kernel thread directly set priority to 0, the macro
    SUGOV_KTHREAD_PRIORITY is not used.  So remove it.
    
    Signed-off-by: Leo Yan <leo.yan@linaro.org>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J . Wysocki <rafael.j.wysocki@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vikram Mulukutla <markivx@codeaurora.org>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Link: http://lkml.kernel.org/r/1518097702-9665-1-git-send-email-leo.yan@linaro.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Thanuj <thanujbalamurali09@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -26,7 +26,6 @@ unsigned long boosted_cpu_util(int cpu);
 #define cpufreq_driver_fast_switch(x, y) 0
 #define cpufreq_enable_fast_switch(x)
 #define cpufreq_disable_fast_switch(x)
-#define SUGOV_KTHREAD_PRIORITY	50
 #define UP_RATE_LIMIT_US	(500)
 #define DOWN_RATE_LIMIT_US	(20000)
 
